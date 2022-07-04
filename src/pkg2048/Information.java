440920b24c61740bdb4daebf20a7aab59c230fc2
SolisCommet <96927353+SolisCommet@users.noreply.github.com>
2022-07-04 14:33:38 +0700

    Class to store information

---
+++ src/pkg2048/Information.java
@@ -0,0 +1,27 @@
+package pkg2048;
+
+/**
+ *
+ * @author Pham Nhat Quang
+ */
+public class Information {
+
+    private int score; //Current game
+    private int bestScore;
+    private int totalScore;
+
+    //512
+    private int gameReached512;
+    private int shortestTime512;
+    private int fewestMoves512;
+
+    private int time512;
+    //1024
+    private int gameReached1024;
+    private int shortestTime1024;
+    private int fewestMoves1024;
+
+    private int time1024;
+
+    private int numOfMoves;
+}
